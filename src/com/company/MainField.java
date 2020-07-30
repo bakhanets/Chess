11adc408ed74165f5e4b893e13387ca713486c78
Andrew G <glazynovand@gmail.com>
2020-07-30 22:57:57 +0300

    add board color

--- src/com/company/MainField.java
+++ src/com/company/MainField.java
@@ -19,13 +19,13 @@ public class MainField extends JFrame {
                 System.out.println(e.getX() + "," + e.getY());
             }
         });
-
-
     }
 
     public void paint(Graphics g) {
         super.paint(g);  // fixes the immediate problem.
         Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(new Color(10, 100, 20));
+
         for (int i = 0; i < 8; i++) {
             Line2D lineVertical = new Line2D.Float(100 * i, 0, 100 * i, 820);
             g2.draw(lineVertical);
@@ -33,7 +33,16 @@ public class MainField extends JFrame {
             Line2D lineHorizontal = new Line2D.Float(0, 100 * i+20, 800, 100 * i + 20);
             g2.draw(lineHorizontal);
 
-
+            for (int j = 0; j < 8; j++) {
+                if ((j + i) % 2 != 0)
+                    g2.fillRect(100 * i, j * 100 + 20, 800 / 8, 820 / 8);
+            }
         }
+
+        int squareX = 50;
+        int squareY = 50;
+        int squareW = 20;
+        int squareH = 20;
+
     }
 }
